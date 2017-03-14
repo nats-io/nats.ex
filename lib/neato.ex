a6099bfa9f13b8b6d07b9858ffcc9c24fa5ab475
Michael Ries <michael@riesd.com>
2017-03-14 14:20:58 -0600

    basics of pub and sub

--- lib/neato.ex
+++ lib/neato.ex
@@ -19,42 +19,65 @@ defmodule Neato do
 
   def stop(pid), do: GenServer.call(pid, :stop)
 
+  def sub(pid, subscriber, topic), do: GenServer.call(pid, {:sub, subscriber, topic})
+
+  def pub(pid, topic, message), do: GenServer.call(pid, {:pub, topic, message})
+
   def init(connection_settings) do
     connection_settings = Map.merge(@default_connection_settings, connection_settings)
     {:ok, tcp} = :gen_tcp.connect(connection_settings.host, connection_settings.port, connection_settings.tcp_opts)
-    case perform_handshake(tcp, :waiting_for_info) do
+    case perform_handshake(tcp) do
       :ok ->
-        {:ok, %{tcp: tcp, connection_settings: connection_settings, state: :waiting_for_message}}
+        {:ok, %{tcp: tcp, connection_settings: connection_settings, state: :waiting_for_message, next_sid: 1, receivers: %{}}}
       {:error, reason} ->
         :gen_tcp.close(tcp)
         {:error, reason}
     end
   end
 
-  def handle_info({:tcp, tcp, "MSG"<>rest}, %{tcp: tcp, state: :waiting_for_message}=state) do
-    Logger.info "#{__MODULE__} received: MSG#{rest}"
+  def handle_info({:tcp, tcp, "MSG "<>rest}, %{tcp: tcp, state: :waiting_for_message}=state) do
+    [cmd | msg_chunks] = String.split(rest, "\r\n")
+    [topic, sid_str, size_str] = String.split(cmd, " ")
+    msg = Enum.join(msg_chunks, "\r\n") |> String.strip
+    sid = String.to_integer(sid_str)
+    send state.receivers[sid], {:msg, topic, msg}
     {:noreply, state}
   end
+  def handle_info(other, state) do
+    Logger.error "#{__MODULE__} received unexpected message: #{inspect other}"
+    {:noreply, state}
+  end
+
 
   def handle_call(:stop, _from, state) do
     :gen_tcp.close(state.tcp)
     {:stop, :normal, :ok, state}
   end
+  def handle_call({:sub, receiver, topic}, _from, %{next_sid: sid}=state) do
+    :ok = :gen_tcp.send(state.tcp, ["SUB ", topic, " #{sid}\r\n"])
+    receivers = Map.put(state.receivers, sid, receiver)
+    next_state = Map.merge(state, %{receivers: receivers, next_sid: sid + 1})
+    {:reply, :ok, next_state}
+  end
+  def handle_call({:pub, topic, message}, _from, state) do
+    publish_data = [["PUB ", topic, " #{IO.iodata_length(message)}\r\n"], [message, "\r\n"]]
+    :ok = :gen_tcp.send(state.tcp, publish_data)
+    {:reply, :ok, state}
+  end
 
-  defp perform_handshake(tcp, :waiting_for_info) do
+  defp perform_handshake(tcp) do
     receive do
       {:tcp, ^tcp, "INFO"<>_} ->
-        :gen_tcp.send(tcp, "CONNECT {}\r\n")
-        perform_handshake(tcp, :waiting_for_ack)
+        :gen_tcp.send(tcp, "CONNECT {\"verbose\": false}\r\n")
       after 1000 ->
         {:error, "timed out waiting for info"}
     end
   end
-  defp perform_handshake(tcp, :waiting_for_ack) do
+
+  defp wait_for_ack(tcp, timeout) do
     receive do
       {:tcp, ^tcp, "+OK\r\n"} -> :ok
-      after 1000 ->
-        {:error, "timed out waiting for connection ack"}
+      after timeout -> {:error, :timeout}
     end
   end
 end
--- test/neato_test.exs
+++ test/neato_test.exs
@@ -7,4 +7,15 @@ defmodule NeatoTest do
     assert Process.alive?(pid)
     :ok = Neato.stop(pid)
   end
+
+  test "subscribe to topic and receive a message" do
+    {:ok, pid} = Neato.start_link()
+    :ok = Neato.sub(pid, self(), "test")
+    :ok = Neato.pub(pid, "test", "yo dawg")
+    :ok = Neato.pub(pid, "test", "hi momma")
+
+    assert_receive {:msg, "test", "yo dawg"}, 1000
+    assert_receive {:msg, "test", "hi momma"}, 1000
+    :ok = Neato.stop(pid)
+  end
 end
