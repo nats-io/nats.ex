b418beebe56a155c1471a091b9e1023353088a7e
Michael Ries <michael@riesd.com>
2017-03-15 20:40:40 -0600

    grab some quick benchmarks

--- README.md
+++ README.md
@@ -17,3 +17,14 @@ Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_do
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/neato](https://hexdocs.pm/neato).
 
+## Benchmarks
+
+Part of the motivation for building this library is to get better performance.
+To this end I've started a `bench.exs` script that we can use to check our performance.
+As of this commit the most recent numbers from running on my macbook pro are:
+
+|   | ips | average | deviation | median |
+| - | --- | ------- | --------- | ------ |
+| parse-128 | 90.54 K | 11.04 μs | ±78.18% | 10.00 μs |
+| pub - 128 | 88.03 K | 11.36 μs | ±82.08% | 11.00 μs |
+| subpub-16 | 7.44 K | 134.49 μs | ±47.57% | 122.00 μs |
--- bench.exs
+++ bench.exs
@@ -1,17 +1,30 @@
 {:ok, pid} = Neato.start_link(%{host: '127.0.0.1', port: 4222})
 
 msg128="74c93e71c5aa03ad4f0881caa374ba1af08f3e4a04ce5f8bd0b2d82d6d72de6eef3e46ed8d8c3dbe24d0f6109115dcdf13280d1c13c2f6d22d14336b29df8e65"
+msg16="74c93e71c5aa03ad"
+
+IO.puts "== Parsing tcp packets"
+tcp_packet = "MSG topic 1 128\r\n#{msg128}\r\n"
+Benchee.run(%{
+  "parse-128" => fn -> {_parser, [_msg]} = Neato.Parser.new() |> Neato.Parser.parse(tcp_packet) end
+}, time: 5)
+
+IO.puts "== Publish Throughput"
 Benchee.run(%{
   "pub - 128" => fn -> :ok = Neato.pub(pid, "pub128", msg128) end,
-  "pubsub - 128" => fn ->
+}, time: 5)
+
+IO.puts "== Subscribe -> Publish -> Receive Throughput"
+Benchee.run(%{
+  "subpub-16" => fn ->
     rand = :crypto.strong_rand_bytes(4) |> Base.encode64
     :ok = Neato.sub(pid, self(), rand)
-    :ok = Neato.pub(pid, rand, msg128)
+    :ok = Neato.pub(pid, rand, msg16)
     receive do
-      {:msg, ^rand, ^msg128} -> :ok
+      {:msg, ^rand, ^msg16} -> :ok
       after 100 -> raise "timed out on sub"
     end
   end,
-}, time: 5)
+}, time: 10)
 
 Neato.stop(pid)
