05494213e4fbfffc2dd8c507fda6bc0a884e7b04
Dave Hackett <tallguy.hackett@gmail.com>
2017-03-30 23:01:20 -0600

    Add functionality to test_helper to verify a server requiring authentication is running

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -11,3 +11,16 @@ case :gen_tcp.connect('localhost', 4222, [:binary]) do
               " #{:inet.format_error(reason)}\n" <>
               "You probably need to start gnatsd."
 end
+
+case :gen_tcp.connect('localhost', 4223, [:binary]) do
+  {:ok, socket} ->
+    :gen_tcp.close(socket)
+  {:error, reason} ->
+    Mix.raise "Cannot connect to gnatsd" <>
+              " (http://localhost:4223):" <>
+              " #{:inet.format_error(reason)}\n" <>
+              "You probably need to start a gnatsd " <>
+              "server that requires authentication with " <>
+              "the following command `gnatsd -p 4223 " <>
+              "--user bob --pass alice`."
+end
