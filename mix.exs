cc2106cd8291418fbc8a5208b2f9cda23296e220
Michael Ries <michael@riesd.com>
2017-04-14 06:20:27 -0600

    update docs config

--- mix.exs
+++ mix.exs
@@ -2,12 +2,19 @@ defmodule Gnat.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :gnat,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :gnat,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      docs: [
+        main: "readme",
+        logo: "gnat.png",
+        extras: ["README.md"],
+      ]
+    ]
   end
 
   def application do
