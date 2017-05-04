24b5550881e22b8dc1cdeb93767985b92d85df8c
Michael Ries <michael@riesd.com>
2017-05-03 23:32:17 -0600

    add documentation for Gnat.ConnectionSupervisor

--- lib/gnat/connection_supervisor.ex
+++ lib/gnat/connection_supervisor.ex
@@ -2,6 +2,33 @@ defmodule Gnat.ConnectionSupervisor do
   use GenServer
   require Logger
 
+  @moduledoc """
+  A process that can supervise a named connection for you
+
+  If you would like to supervise a Gnat connection and have it automatically re-connect in case of failure you can use this module in your supervision tree.
+  It takes a map with the following data:
+
+  ```
+  gnat_supervisor_settings = %{
+    name: :gnat, # (required) the registered named you want to give the Gnat connection
+    backoff_period: 4_000, # number of milliseconds to wait between consecurity reconnect attempts (default: 2_000)
+    connection_settings: [
+      %{host: '10.0.0.100', port: 4222},
+      %{host: '10.0.0.101', port: 4222},
+    ]
+  }
+  ```
+
+  The connection settings can specify all of the same values that you pass to `Gnat.start_start_link/1`. Each time a connection is attempted we will use one of the provided connection settings to open the connection. This is a simplistic way of load balancing your connections across a cluster of nats nodes and allowing failove to other nodes in the cluster if one goes down.
+
+  To use this in your supervision tree add an entry like this:
+
+  ```
+  import Supervisor.Spec
+  worker(Gnat.ConnectionSupervisor, [gnat_supervisor_settings])
+  ```
+  """
+
   def start_link(options) do
     GenServer.start_link(__MODULE__, options, name: __MODULE__)
   end
@@ -23,7 +50,9 @@ defmodule Gnat.ConnectionSupervisor do
     Logger.info "connecting to #{inspect connection_config}"
     case Gnat.start_link(connection_config, name: state.name) do
       {:ok, gnat} -> {:noreply, %{state | gnat: gnat}}
-      {:error, err} ->{:noreply, %{state | gnat: nil}} # we will get an :EXIT message and handle it there
+      {:error, err} ->
+        Logger.error "failed to connect #{inspect err}"
+        {:noreply, %{state | gnat: nil}} # we will get an :EXIT message and handle it there
     end
   end
   def handle_info({:EXIT, _pid, reason}, %{gnat: nil}=state) do
