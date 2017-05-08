b8978b4360371da94a28b081012f99bd58abed2d
Michael Ries <michael@riesd.com>
2017-05-08 09:39:53 -0600

    release 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gnat.Mixfile do
   def project do
     [
       app: :gnat,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
