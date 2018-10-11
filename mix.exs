2857d97158e477fa1769367a7e999de11e956682
Michael Ries <michael@riesd.com>
2018-10-11 07:55:05 -0600

    bump patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gnat.Mixfile do
   def project do
     [
       app: :gnat,
-      version: "0.5.1",
+      version: "0.5.2",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env == :prod,
