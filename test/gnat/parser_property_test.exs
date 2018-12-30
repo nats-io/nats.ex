b53215a3b651fb88b606dafb15fa8571cf8ba691
Michael Ries <michael@riesd.com>
2018-12-29 22:31:06 -0700

    change the property test to use the new parsec

--- test/gnat/parser_property_test.exs
+++ test/gnat/parser_property_test.exs
@@ -1,14 +1,14 @@
-defmodule Gnat.ParserPropertyTest do
+defmodule Gnat.ParsecPropertyTest do
   use ExUnit.Case, async: true
   use PropCheck
   import Gnat.Generators, only: [message: 0, protocol_message: 0]
-  alias Gnat.Parser
+  alias Gnat.Parsec
   @numtests (System.get_env("N") || "100") |> String.to_integer
 
   @tag :property
   property "can parse any message" do
     numtests(@numtests * 10, forall map <- message() do
-      {_parser, [parsed]} = Parser.new() |> Parser.parse(map.binary)
+      {_parser, [parsed]} = Parsec.new() |> Parsec.parse(map.binary)
       {:msg, parsed_subject, parsed_sid, parsed_reply_to, parsed_payload} = parsed
       parsed_payload == map.payload &&
       parsed_subject == map.subject &&
@@ -22,13 +22,13 @@ defmodule Gnat.ParserPropertyTest do
     numtests(@numtests, forall messages <- list(message()) do
       frames = messages |> Enum.reduce("", fn(%{binary: b},acc) -> acc<>b end) |> random_chunk
       payloads = Enum.map(messages, fn(msg) -> msg.payload end)
-      {parser, parsed} = Enum.reduce(frames, {Parser.new(), []}, fn(frame, {parser, acc}) ->
-        {parser, parsed_messages} = Parser.parse(parser, frame)
+      {parser, parsed} = Enum.reduce(frames, {Parsec.new(), []}, fn(frame, {parser, acc}) ->
+        {parser, parsed_messages} = Parsec.parse(parser, frame)
         payloads = Enum.map(parsed_messages, &( elem(&1, 4) ))
         {parser, acc ++ payloads}
       end)
 
-      parser.partial == "" &&
+      parser.partial == nil &&
       parsed == payloads
     end)
   end
@@ -36,11 +36,11 @@ defmodule Gnat.ParserPropertyTest do
   @tag :property
   property "can parse any sequence of protocol messages without exception" do
     numtests(@numtests, forall messages <- list(protocol_message()) do
-      parser = Enum.reduce(messages, Parser.new(), fn(%{binary: bin}, parser) ->
-        {parser, [_parsed]} = Parser.parse(parser, bin)
+      parser = Enum.reduce(messages, Parsec.new(), fn(%{binary: bin}, parser) ->
+        {parser, [_parsed]} = Parsec.parse(parser, bin)
         parser
       end)
-      parser.partial == ""
+      parser.partial == nil
     end)
   end
 
@@ -49,11 +49,11 @@ defmodule Gnat.ParserPropertyTest do
     numtests(@numtests, forall messages <- list(protocol_message()) do
       bin = Enum.reduce(messages, "", fn(%{binary: part}, acc) -> acc<>part end)
       chunks = random_chunk(bin)
-      {parser, parsed} = Enum.reduce(chunks, {Parser.new(), []}, fn(bin, {parser, acc}) ->
-        {parser, parsed} = Parser.parse(parser, bin)
+      {parser, parsed} = Enum.reduce(chunks, {Parsec.new(), []}, fn(bin, {parser, acc}) ->
+        {parser, parsed} = Parsec.parse(parser, bin)
         {parser, acc ++ parsed}
       end)
-      parser.partial == "" &&
+      parser.partial == nil &&
       Enum.count(parsed) == Enum.count(messages)
     end)
   end
