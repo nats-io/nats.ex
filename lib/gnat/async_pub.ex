49b4bce4dc89412b57659cd02dc1688bd1c1e2e5
mmmries <riesmmm@gmail.com>
2020-02-18 21:36:12 -0500

    add graceful shutdown

--- lib/gnat/async_pub.ex
+++ lib/gnat/async_pub.ex
@@ -5,42 +5,48 @@ defmodule Gnat.AsyncPub do
   @default_queue_settings %{max_messages: 10_000, max_memory: 100 * 1024 * 1024}
   @max_in_flight 10
   @pause 25
+  @shutting_down_status :shutting_down
 
   def start_link(queue_settings \\ %{}, opts \\ []) do
     GenServer.start_link(__MODULE__, queue_settings, opts)
   end
 
-  @spec pub(GenServer.server, String.t, binary(), keyword()) :: :ok
+  @type pub_error :: :shutting_down | :queue_full | :memory_full
+  @spec pub(GenServer.server, String.t, binary(), keyword()) :: :ok | {:error, pub_error()}
   def pub(pid, topic, message, opts \\ []) do
     GenServer.call(pid, {:pub, topic, message, opts})
   end
 
-  def stop(pid), do: GenServer.call(pid, :stop)
-
   @impl GenServer
   def init(%{connection_name: name}=queue_settings) when is_atom(name) do
+    Process.flag(:trap_exit, true)
+    Process.flag(:trap_exit, true)
     queue_settings =
       @default_queue_settings
       |> Map.merge(queue_settings)
       |> Map.put(:queue, :queue.new())
       |> Map.put(:in_flight, %{})
+      |> Map.put(:status, :running)
     {:ok, queue_settings, 0}
   end
 
   @impl GenServer
+  def handle_call({:pub, _top, _msg, _opts}, _from, %{status: @shutting_down_status}=state) do
+    {:reply, {:error, :shutting_down}, state, 0}
+  end
   def handle_call({:pub, _topic, _message, _opts}=todo, _from, state) do
     state = update_in(state, [:queue], fn(q) -> :queue.in(todo, q) end)
     {:reply, :ok, state, 0}
-  end
-  def handle_call(:stop, _from, state) do
-    # wait for queue to empty?
-    {:stop, :normal, :ok, state}
   end
 
   @impl GenServer
   def handle_info(:timeout, state) do
     if :queue.is_empty(state.queue) do
-      {:noreply, state, @pause}
+      if state.status == @shutting_down_status do
+        {:stop, :normal, state}
+      else
+        {:noreply, state, @pause}
+      end
     else
       {state, timeout} = try_to_send_message(state)
       {:noreply, state, timeout}
@@ -50,9 +56,24 @@ defmodule Gnat.AsyncPub do
     in_flight = Map.delete(state.in_flight, ref)
     {:noreply, %{state | in_flight: in_flight}, 0}
   end
+  def handle_info(:shutdown, state) do
+    Logger.info("#{__MODULE__} starting graceful shutdown")
+    {:noreply, %{state | status: @shutting_down_status}, 0}
+  end
   def handle_info(other, state) do
     Logger.error("#{__MODULE__} Received Unexpected Message: #{inspect(other)}")
-    {:noreply, state}
+    {:noreply, state, 0}
+  end
+
+  @impl GenServer
+  def terminate(:shutdown, _state) do
+    Process.send(self(), :shutdown, [])
+  end
+  def terminate(:normal, _state) do
+    Logger.info("#{__MODULE__} finished graceful shutdown")
+  end
+  def terminate(reason, _state) do
+    Logger.error("#{__MODULE__} unexpected shutdown #{inspect(reason)}")
   end
 
   defp try_to_send_message(%{queue: queue, connection_name: name, in_flight: in_flight}=state) do
--- test/async_pub_test.exs
+++ test/async_pub_test.exs
@@ -26,4 +26,29 @@ defmodule Gnat.AsyncPubTest do
     # anonymous connection
     assert_receive {:msg, %{topic: "async_pub_t2", body: "Ron Swanson", reply_to: "me"}}, 1000
   end
+
+  @tag capture_log: true
+  test "a graceful shutdown is attempted when being shutdown by a supervisor" do
+    # start anonymous connection and subscribe to topic
+    {:ok, pid} = Gnat.start_link()
+    {:ok, _ref} = Gnat.sub(pid, self(), "async_pub_t3")
+
+    # start async pub and send it a message before its connection is available
+    {:ok, queue} = AsyncPub.start_link(%{connection_name: :async_pub_c2}, name: :async_pub_q2)
+    assert :ok = AsyncPub.pub(:async_pub_q2, "async_pub_t3", "Ron Swanson", reply_to: "me")
+
+    # initiate a graceful shutdown
+    Process.send(queue, :shutdown, []) # why does Process.exit kill the test? It should be trapped by
+    assert {:error, :shutting_down} = AsyncPub.pub(:async_pub_q2, "async_pub_t3", "Ron Swanson", reply_to: "me")
+
+    # start the connection that our async pub wants to use
+    {:ok, _pid} = Gnat.start_link(%{}, name: :async_pub_c2)
+
+    # it should have used the new connection to publish our message and then we get it from our
+    # anonymous connection
+    assert_receive {:msg, %{topic: "async_pub_t3", body: "Ron Swanson", reply_to: "me"}}, 1000
+
+    # The graceful shutdown should be complete at this point
+    refute Process.alive?(queue)
+  end
 end
