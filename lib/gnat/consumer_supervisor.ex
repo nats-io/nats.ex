142667b09f792a23212164b911eb69ef133ca23d
mmmries <riesmmm@gmail.com>
2020-10-08 16:39:32 -0600

    add support for Gnat.Server modules in ConsumerSupervisor

--- lib/gnat/consumer_supervisor.ex
+++ lib/gnat/consumer_supervisor.ex
@@ -5,14 +5,16 @@ defmodule Gnat.ConsumerSupervisor do
   @moduledoc """
   A process that can supervise consumers for you
 
-  If you want to subscribe to a few topics and have that subscription last across restarts for you, then this worker can be of help. It also spawns a supervised `Task` for each message it receives. This way errors in message processing don't crash the consumers, but you will still get SASL reports that you can send to services like honeybadger.
+  If you want to subscribe to a few topics and have that subscription last across restarts for you, then this worker can be of help.
+  It also spawns a supervised `Task` for each message it receives.
+  This way errors in message processing don't crash the consumers, but you will still get SASL reports that you can send to services like honeybadger.
 
   To use this just add an entry to your supervision tree like this:
 
   ```
   consumer_supervisor_settings = %{
     connection_name: :name_of_supervised_connection,
-    consuming_function: {MyApp.RpcServer, :handle_request},
+    module: MyApp.Server, # a module that implements the Gnat.Server behaviour
     subscription_topics: [
       %{topic: "rpc.MyApp.search", queue_group: "rpc.MyApp.search"},
       %{topic: "rpc.MyApp.create", queue_group: "rpc.MyApp.create"},
@@ -24,6 +26,9 @@ defmodule Gnat.ConsumerSupervisor do
   The second argument is a keyword list that gets used as the GenServer options so you can pass a name that you want to register for the consumer process if you like. The `:consuming_function` specifies which module and function to call when messages arrive. The function will be called with a single argument which is a `t:Gnat.message/0` just like you get when you call `Gnat.sub/4` directly.
 
   You can have a single consumer that subscribes to multiple topics or multiple consumers that subscribe to different topics and call different consuming functions. It is recommended that your `ConsumerSupervisor`s are present later in your supervision tree than your `ConnectionSupervisor`. That way during a shutdown the `ConsumerSupervisor` can attempt a graceful shutdown of the consumer before shutting down the connection.
+
+  It's also possible to pass a `%{consuming_function: {YourModule, :your_function}}` rather than a `:module` in your settings.
+  In that case no error handling or replying is taking care of for you, it will be up to your function to take whatever action you want with each message.
   """
   @spec start_link(map(), keyword()) :: GenServer.on_start
   def start_link(settings, options \\ []) do
@@ -36,18 +41,30 @@ defmodule Gnat.ConsumerSupervisor do
     {:ok, task_supervisor_pid} = Task.Supervisor.start_link()
     connection_name = Map.get(settings, :connection_name)
     subscription_topics = Map.get(settings, :subscription_topics)
-    consuming_function = Map.get(settings, :consuming_function)
-    send self(), :connect
+
     state = %{
       connection_name: connection_name,
       connection_pid: nil,
-      consuming_function: consuming_function,
       status: :disconnected,
       subscription_topics: subscription_topics,
       subscriptions: [],
       task_supervisor_pid: task_supervisor_pid,
     }
-    {:ok, state}
+
+    cond do
+      Map.has_key?(settings, :module) ->
+        state = Map.put(state, :module, settings.module)
+        send self(), :connect
+        {:ok, state}
+
+      Map.has_key?(settings, :consuming_function) ->
+        state = Map.put(state, :consuming_function, settings.consuming_function)
+        send self(), :connect
+        {:ok, state}
+
+      true ->
+        {:error, "You must provide a module or consuming function for the consumer supervisor"}
+    end
   end
 
   @impl GenServer
@@ -80,6 +97,11 @@ defmodule Gnat.ConsumerSupervisor do
   def handle_info({:EXIT, supervisor_pid, _reason}, %{task_supervisor_pid: supervisor_pid}=state) do
     {:ok, task_supervisor_pid} = Task.Supervisor.start_link()
     {:noreply, Map.put(state, :task_supervisor_pid, task_supervisor_pid)}
+  end
+
+  def handle_info({:msg, gnat_message}, %{module: module} = state) do
+    Task.Supervisor.async_nolink(state.task_supervisor_pid, Gnat.Server, :execute, [module, gnat_message])
+    {:noreply, state}
   end
 
   def handle_info({:msg, gnat_message}, %{consuming_function: {mod, fun}}=state) do
