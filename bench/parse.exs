7d7a317000c4d821119bda31d2f2cb1ab79909b3
mmmries <riesmmm@gmail.com>
2021-06-12 22:27:33 -0700

    add benchmark for parsing messages with headers

--- bench/parse.exs
+++ bench/parse.exs
@@ -6,6 +6,7 @@ inputs = %{
   "16 byte" => "MSG topic 1 16\r\n#{msg16}\r\n",
   "128 byte" => "MSG topic 1 128\r\n#{msg128}\r\n",
   "1024 byte" => "MSG topic 1 1024\r\n#{msg1024}\r\n",
+  "7 byte with headers" => "HMSG SUBJECT 1 REPLY 48 55\r\nNATS/1.0\r\nHeader1: X\r\nHeader1: Y\r\nHeader2: Z\r\n\r\nPAYLOAD\r\n"
 }
 
 parsec = Gnat.Parsec.new()
