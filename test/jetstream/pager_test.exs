cd2216cb05d768998a034d76fb65fff148eb00e1
Michael Ries <mries@salesforce.com>
2024-04-25 13:27:51 -0700

    add reduce function and a unit test

---
+++ test/jetstream/pager_test.exs
@@ -0,0 +1,34 @@
+defmodule Gnat.Jetstream.PagerTest do
+  use Gnat.Jetstream.ConnCase
+  alias Gnat.Jetstream.Pager
+  alias Gnat.Jetstream.API.Stream
+
+  @moduletag with_gnat: :gnat
+
+  test "paging over a simple stream" do
+    {:ok, _stream} = create_stream("pager_a")
+    Enum.each(1..100, fn i ->
+      :ok = Gnat.pub(:gnat, "input.pager_a", "#{i}")
+    end)
+
+    {:ok, res} = Pager.reduce(:gnat, "pager_a", [from_seq: 1], 0, fn msg, total ->
+      total + String.to_integer(msg.body)
+    end)
+    assert res == 5050
+
+    {:ok, res} = Pager.reduce(:gnat, "pager_a", [from_seq: 51], 0, fn msg, total ->
+      total + String.to_integer(msg.body)
+    end)
+    assert res == 3775
+
+    Stream.delete(:gnat, "pager_a")
+  end
+
+  defp create_stream(name) do
+    stream = %Stream{
+      name: name,
+      subjects: ["input.#{name}"]
+    }
+    Stream.create(:gnat, stream)
+  end
+end
